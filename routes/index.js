7a00975dc92ed8deb4fac0cf95d112fa83c4adff
yrr626 <437548960@qq.com>
2017-07-28 16:56:19 +0800

    web端首页修改

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 var swig = require('swig');
 router.get('/', function(req, res, next) {
-	var page ;
-	page = swig.renderFile('views/dailyArt.html', {
-		html: "",
-		user: {}
-	});
-	res.send(page);
+	var deviceAgent = req.headers['user-agent'].toLowerCase();
+	if(/mobile/i.test(deviceAgent)){
+        var page ;
+        page = swig.renderFile('views/dailyArt.html', {
+            html: "",
+            user: {}
+        });
+        res.send(page);
+	}else{
+        res.redirect('mng/login.html');
+	}
+
 });
 
 module.exports = router;
